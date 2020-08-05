3af93f5a2cabc3d82cb462e365dfe2bcdfdd8bc8
jonatanva10 <jonatanva10@gmail.com>
2020-08-04 19:36:54 -0600

    Add files via upload

---
+++ test_bisiesto.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func calculiBisects(n int) bool {
+
+	if (n % 4 == 0 && n % 100 != 0) || (n % 100 == 0 && n % 400 == 0) {
+		//System.out.println("El año " + anio + " es bisiesto");
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func plus(a int) int {
+	return a * 5
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	year, err := strconv.Atoi(request.URL.Path[1:])
+	if err == nil {
+		if calculiBisects(year) {
+			fmt.Fprintf(writer, "El año: %s es bisiesto", request.URL.Path[1:])
+		}else{
+			fmt.Fprintf(writer, "El año: %s no es bisiesto", request.URL.Path[1:])
+		}
+	} else{
+		fmt.Fprintf(writer, "Parametros incorrectos")
+	}
+}
+
+func main() {
+	fmt.Println("Calculate Bisiesto")
+
+	fmt.Print("Enter year: ")
+	var input int
+	var otherResult bool
+	fmt.Scanln(&input)
+
+	otherResult = calculiBisects(input)
+	fmt.Print(otherResult)
+}
