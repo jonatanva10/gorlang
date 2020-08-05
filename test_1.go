935d53eb8ade7caf119562780d1aa69c98801413
jonatanva10 <jonatanva10@gmail.com>
2020-08-04 19:21:47 -0600

    Update test_1.go

--- test_1.go
+++ test_1.go
@@ -8,16 +8,15 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-    //num := request.URL.Path[1:]
-    anno, err := strconv.Atoi(request.URL.Path[1:])
-    if err == nil {
-	    if calculiBisects(anno) {
-		    fmt.Fprintf(writer, "El año: %s es bisiesto", request.URL.Path[1:])
-     		}else{
-             	    fmt.Fprintf(writer, "El año: %s no es bisiesto", request.URL.Path[1:])
-	    }
-  }
-	else{
+	//num := request.URL.Path[1:]
+	year, err := strconv.Atoi(request.URL.Path[1:])
+	if err == nil {
+		if calculiBisects(year) {
+			fmt.Fprintf(writer, "El año: %s es bisiesto", request.URL.Path[1:])
+		}else{
+			fmt.Fprintf(writer, "El año: %s no es bisiesto", request.URL.Path[1:])
+		}
+	} else{
 		fmt.Fprintf(writer, "Parametros incorrectos")
 	}
 }
