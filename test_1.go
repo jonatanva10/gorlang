9a5021726e1a2227d0581dd57f37550314369b48
jonatanva10 <jonatanva10@gmail.com>
2020-08-04 19:10:27 -0600

    Update test_1.go

--- test_1.go
+++ test_1.go
@@ -8,9 +8,15 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-    num := request.URL.Path[1:]
+    //num := request.URL.Path[1:]
 	 anno, err := strconv.Atoi(request.URL.Path[1:])
-	fmt.Fprintf(writer, calculiBisects(anno), request.URL.Path[1:])
+	if err == nil {
+     if calculiBisects(anno) {
+             fmt.Fprintf(writer, "El año: %s es bisiesto", anno)
+     }else{
+             fmt.Fprintf(writer, "El año: %s no es bisiesto", anno)
+          }
+  }
 }
 
 func calculiBisects(n int) string {
