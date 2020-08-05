6a64e74a8839dd6fd908326ec162d4308e33b320
jonatanva10 <jonatanva10@gmail.com>
2020-08-04 19:02:11 -0600

    Update test_1.go

--- test_1.go
+++ test_1.go
@@ -7,7 +7,20 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+    num := request.URL.Path[1:]
+    fmt.Fprintf(writer, calculiBisects(num), request.URL.Path[1:])
+}
+
+func calculiBisects(n int) string {
+	d := "Sin Datos"
+	if (n % 4 == 0 && n % 100 != 0) || (n % 100 == 0 && n % 400 == 0) {
+		
+		d  = "Año Bisiesto"
+	} else {
+		d  = "Año No Bisiesto"
+	}
+
+	return d
 }
 
 func main() {
